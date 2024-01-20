907debb8f4b28ba96500e62652eee13f339ebc7b
dibyendu <dibyendu.me19@gmail.com>
2024-01-20 08:19:14 +0100

    adding test case for app.component.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from './app.component'
+import { By } from '@angular/platform-browser'
+import { Observable, of } from 'rxjs'
+import { NavigationEnd, Router } from '@angular/router'
+
+class MockServices {
+  public events = of( new NavigationEnd(0, 'http://localhost:4200/login', 'http://localhost:4200/login'))
+  public navigate = jasmine.createSpy('navigate')
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -11,19 +19,49 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  });
+      providers: [
+        { provide: Router, useClass: MockServices },
+      ]
+    }).compileComponents()
+  })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
 
   it(`should have as title 'pesto-todo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pesto-todo');
-  });
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app.title).toEqual('pesto-todo')
+  })
+
+  it(`should have log out button hidden without auth`, () => {
+    const fixture = TestBed.createComponent(AppComponent),
+    button = fixture.debugElement.query(By.css('.btn'))
+    expect(button).toBeNull()
+  })
+
+  it(`should have log out button after auth and can logout`, fakeAsync(() => {
+    sessionStorage.setItem('user', 'test')
+    const fixture = TestBed.createComponent(AppComponent),
+    component = fixture.componentInstance
+    fixture.detectChanges()
+    spyOn(component, 'logOut')
+    const btn = fixture.debugElement.query(By.css('.btn'))
+    btn.triggerEventHandler('click', null)
+    tick()
+    fixture.detectChanges()
+    expect(component.logOut).toHaveBeenCalled()
+  }))
+
+  it(`should clear the user from logout click`, fakeAsync(() => {
+    sessionStorage.setItem('user', 'test')
+    const fixture = TestBed.createComponent(AppComponent),
+    component = fixture.componentInstance
+    component.logOut()
+    expect(sessionStorage.getItem('user')).toBeNull()
+  }))
 
 });
